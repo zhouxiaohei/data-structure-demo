3a06aa5fd18a9933e587820ce605747cfbbf9d15
jackZhou <zhouzhen01@gridsum.com>
2021-02-18 18:17:45 +0800

    测试代码

--- src/main/java/com/zhou/demo/test/QueueDemo.java
+++ src/main/java/com/zhou/demo/test/QueueDemo.java
@@ -1,7 +1,7 @@
 package com.zhou.demo.test;
 
+import java.util.Scanner;
 import java.util.Stack;
-import java.util.TreeMap;
 
 /**
  * @ClassName QueueDemo
@@ -9,19 +9,51 @@ import java.util.TreeMap;
  * @Date 2021/1/14  19:22
  **/
 public class QueueDemo {
-    private Stack<String> stackA;
-    private Stack<String> stackB;
+    private Stack<Integer> stackA;
+    private Stack<Integer> stackB;
 
     public static void main(String[] args) {
-        TreeMap map = new TreeMap();
+        QueueDemo queue = new QueueDemo();
+        char key = ' '; // 接收用户输入
+        Scanner scanner = new Scanner(System.in);//
+        boolean loop = true;
+        while (loop) {
+            System.out.println("e(exit): 退出程序");
+            System.out.println("a(add): 添加数据到队列");
+            System.out.println("g(get): 从队列取出数据");
+            key = scanner.next().charAt(0);// 接收一个字符
+            switch (key) {
+                case 'a':
+                    System.out.println("输出一个数");
+                    int value = scanner.nextInt();
+                    queue.put(value);
+                    break;
+                case 'g': // 取出数据
+                    try {
+                        int res = queue.take();
+                        System.out.printf("取出的数据是%d\n", res);
+                    } catch (Exception e) {
+                        // TODO: handle exception
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case 'e': // 退出
+                    scanner.close();
+                    loop = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+        System.out.println("程序退出~~");
     }
 
     public QueueDemo(){
-        stackA = new Stack<String>();
-        stackB = new Stack<String>();
+        stackA = new Stack<Integer>();
+        stackB = new Stack<Integer>();
     }
 
-    public void push(String message){
+    public void put(Integer message){
         while(!stackB.empty()){
             stackA.push(stackB.pop());
         }
@@ -29,8 +61,10 @@ public class QueueDemo {
         stackB.clear();
     }
 
-    public String get(){
-        stackB.addAll(stackA);
+    public Integer take(){
+        while(!stackA.empty()){
+            stackB.push(stackA.pop());
+        }
         stackA.clear();
         return stackB.pop();
     }
